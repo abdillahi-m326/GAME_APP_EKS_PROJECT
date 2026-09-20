dcb9c10ae579e522ec7de504dff1a242e352f971
Abdillahi Mirie <abdimirie326@gmail.com>
2026-09-20 14:33:24 +0100

    Adding helm provider

--- terraform/modules/irsa/main.tf
+++ terraform/modules/irsa/main.tf
@@ -15,16 +15,17 @@ module "irsa" {
 tags = var.tags
 }
 
-module "Externalirsa" {
+# Exteranal-DNS IRSA
+module "external_dns_irsa" {
   source = "terraform-aws-modules/iam/aws//modules/iam-role-for-service-accounts"
 
-  name = "cert-manager-irsa"
-  attach_cert_manager_policy = true
+  name = "external-dns-irsa"
+  attach_external_dns_policy = true
 
   oidc_providers = {
     eks = {
-      provider_arn               = "arn:aws:iam::012345678901:oidc-provider/oidc.eks.us-east-1.amazonaws.com/id/5C54DDF35ER19312844C7333374CC09D"
-      namespace_service_accounts = ["cert-manager:cert-manager"]
+      provider_arn               = module.eks.cluster_oidc_issuer_arn
+      namespace_service_accounts = ["external-dns:external-dns"]
     }
   }
 
