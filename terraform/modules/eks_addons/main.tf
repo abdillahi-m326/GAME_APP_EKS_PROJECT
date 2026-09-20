2c7a22dfa405b658c0ba0dff912674a4b1b50378
Abdillahi Mirie <abdimirie326@gmail.com>
2026-09-20 15:43:04 +0100

    adding nginx ingress helm chart

---
+++ terraform/modules/eks_addons/main.tf
@@ -0,0 +1,8 @@
+resource "helm_release" "nginx_ingress" {
+  name       = "ingress-nginx"
+  repository = "https://kubernetes.github.io/ingress-nginx"
+  chart      = "ingress-nginx"
+  
+  create_namespace = true
+  namespace  = "ingress-nginx"
+}
